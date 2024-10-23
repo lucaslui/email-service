a7cd1d7647db204f6bd629754b4fcc955e3846f7
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-10-23 17:44:43 -0300

    test: add two test suites

---
+++ src/test/java/com/lucaslui/email_service/Controllers/EmailControllerTest.java
@@ -0,0 +1,74 @@
+package com.lucaslui.email_service.Controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentCaptor.forClass;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lucaslui.email_service.Controllers.models.EmailRequestBody;
+import com.lucaslui.email_service.Domain.Models.EmailModel;
+import com.lucaslui.email_service.Services.EmailService;
+
+@SpringBootTest
+public class EmailControllerTest {
+
+    @Mock
+    private EmailService emailService;
+
+    @InjectMocks
+    private EmailController emailController;
+
+    @Nested
+    @DisplayName("sendEmail")
+    class SendEmailTests {
+
+        @Test
+        @DisplayName("should return an EmailModel")
+        public void shouldReturnAnEmailModelIfEverythingIsOk() {
+            EmailRequestBody body = new EmailRequestBody();
+
+            body.setTo("any_email@mail.com");
+            body.setSubject("any subject");
+            body.setBody("any body");
+
+            EmailModel emailMock = mock(EmailModel.class);
+
+            when(emailService.sendEmail(any(EmailModel.class))).thenReturn(emailMock);
+
+            EmailModel emailSaved = emailController.sendEmail(body);
+
+            assert (emailSaved != null);
+        }
+
+        @Test
+        @DisplayName("should create Email model with the same values as the EmailRequestBody")
+        public void shouldCreateEmailModelWithTheSameValuesAsTheEmailRequestBody() {    
+            EmailRequestBody body = new EmailRequestBody();
+
+            body.setTo("any_email@mail.com");
+            body.setSubject("any subject");
+            body.setBody("any body");
+
+            emailController.sendEmail(body);
+
+            ArgumentCaptor<EmailModel> emailCaptor = forClass(EmailModel.class);
+            verify(emailService).sendEmail(emailCaptor.capture());
+
+            EmailModel capturedEmailModel = emailCaptor.getValue();
+
+            assertEquals(body.getTo(), capturedEmailModel.getTo());
+            assertEquals(body.getSubject(), capturedEmailModel.getSubject());
+            assertEquals(body.getBody(), capturedEmailModel.getBody());
+        }
+    }
+}
---
+++ src/test/java/com/lucaslui/email_service/Services/EmailServiceTest.java
@@ -0,0 +1,69 @@
+package com.lucaslui.email_service.Services;
+
+import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lucaslui.email_service.Domain.Enums.EmailStatus;
+import com.lucaslui.email_service.Domain.Models.EmailModel;
+import com.lucaslui.email_service.Infrastructure.Repositories.EmailRepository;
+import com.lucaslui.email_service.Services.Interfaces.EmailProvider;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Mock
+    private EmailProvider emailProvider;
+
+    @Mock
+    private EmailRepository emailRepository;
+
+    @Nested
+    @DisplayName("sendEmail")
+    class SendEmailTests {
+
+        @Test
+        @DisplayName("should return email with status SENT if email is sent successfully")
+        public void shouldReturnEmailWithStatusSentIfEmailIsSentSuccessfully() {
+            EmailModel email = new EmailModel();
+            email.setStatus(EmailStatus.PENDING);
+
+            doNothing().when(emailProvider).sendEmail(email);
+
+            emailService.sendEmail(email);
+
+            verify(emailProvider, times(1)).sendEmail(email);
+            verify(emailRepository, times(1)).save(email);
+            assert email.getStatus() == EmailStatus.SENT;
+            assert email.getCreatedAt() != null;
+            assert email.getUpdatedAt() != null;
+        }
+
+        @Test
+        @DisplayName("should return email with status ERROR if email is not sent successfully")
+        public void shouldReturnEmailWithStatusErrorIfEmailIsNotSentSuccessfully() {
+            EmailModel email = new EmailModel();
+            email.setStatus(EmailStatus.PENDING);
+
+            doThrow(new RuntimeException()).when(emailProvider).sendEmail(email);
+
+            emailService.sendEmail(email);
+
+            verify(emailProvider, times(1)).sendEmail(email);
+            verify(emailRepository, times(1)).save(email);
+            assert email.getStatus() == EmailStatus.ERROR;
+            assert email.getCreatedAt() != null;
+            assert email.getUpdatedAt() != null;
+        }
+    }
+}
